d7a346c822d12b67513974caf5c4515266d1f12c
Carl Westmoreland <beguira86@gmail.com>
2015-10-24 15:57:54 -0400

    Fiddlin' around to get a better grasp of CSS and HTML structure

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,24 @@
 require "etsiest/version"
+require "sinatra/base"
+require "etsy"
+require "pry"
+
+Etsy.api_key = ENV['ETSY_KEY']
 
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Base
+    set :logging, true
+
+
+    get "/search" do
+    	data = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'psychadelic')
+    	
+		erb :tweEtsy, locals: { data: data.result }
+	end
+
+   
+    run! if app_file == $0
+	end
+
+
 end
